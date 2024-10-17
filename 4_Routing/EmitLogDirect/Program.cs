1938a2dbc416b6e714a0aaa80513119323794bcf
Dominik Alkhovik <dominik.alk@hotmail.com>
2024-10-17 18:04:48 +0100

    finish routing tutorial

---
+++ 4_Routing/EmitLogDirect/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+// Create connection to the server
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+// Declare direct exchange
+channel.ExchangeDeclare(exchange: "direct_logs", type: ExchangeType.Direct);
+
+var severity = (args.Length > 0) ? args[0] : "info";
+var message = (args.Length > 1)
+              ? string.Join(" ", args.Skip(1).ToArray())
+              : "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+
+// Publish message to named exchange instead of default exchange 
+channel.BasicPublish(exchange: "direct_logs",
+                     // Define routing key for exchange to utilise
+                     routingKey: severity,
+                     basicProperties: null,
+                     body: body);
+Console.WriteLine($" [x] Sent '{severity}':'{message}'");
---
+++ 4_Routing/ReceiveLogsDirect/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+// Create connection to the server
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+// Declare fanout exchange
+channel.ExchangeDeclare(exchange: "direct_logs", type: ExchangeType.Direct);
+
+// Declare a server-named queue
+var queueName = channel.QueueDeclare().QueueName;
+
+// Exit console if no severity is provided
+if (args.Length < 1)
+{
+    Console.Error.WriteLine("Usage: {0} [info] [warning] [error]",
+                            Environment.GetCommandLineArgs()[0]);
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+    Environment.ExitCode = 1;
+    return;
+}
+
+foreach (var severity in args)
+{
+    // Bind queue to exchange with routing key
+    channel.QueueBind(queue: queueName,
+                      exchange: "direct_logs",
+                      routingKey: severity);
+}
+
+Console.WriteLine(" [*] Waiting for logs.");
+
+// Declare callback to handle incoming messages
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    byte[] body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    var routingKey = ea.RoutingKey;
+    Console.WriteLine($" [x] Received '{routingKey}':'{message}'");
+};
+// Start a basic content-class consumer
+channel.BasicConsume(queue: queueName,
+                     autoAck: true,
+                     consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
