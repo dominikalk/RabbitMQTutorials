9de2f1b0e2149ac8a9088bb235e363fa95da4993
Dominik Alkhovik <dominik.alk@hotmail.com>
2024-10-18 11:54:41 +0100

    finish RPC tutorial

--- 6_RPC/RPCClient/Program.cs
+++ 6_RPC/RPCClient/Program.cs
@@ -1,2 +1,94 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// https://www.rabbitmq.com/tutorials/tutorial-six-dotnet
+
+using System.Collections.Concurrent;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+public class RpcClient : IDisposable
+{
+    private const string QUEUE_NAME = "rpc_queue";
+
+    private readonly IConnection connection;
+    private readonly IModel channel;
+    private readonly string replyQueueName;
+    private readonly ConcurrentDictionary<string, TaskCompletionSource<string>> callbackMapper = new();
+
+    public RpcClient()
+    {
+        // Create connection to the server
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        connection = factory.CreateConnection();
+        channel = connection.CreateModel();
+
+        // Declare a server-named queue
+        replyQueueName = channel.QueueDeclare().QueueName;
+
+        // Declare callback to handle incoming response messages
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
+        {
+            if (!callbackMapper.TryRemove(ea.BasicProperties.CorrelationId, out var tcs))
+                return;
+            var body = ea.Body.ToArray();
+            var response = Encoding.UTF8.GetString(body);
+            tcs.TrySetResult(response);
+        };
+
+        // Subscribe to callback queue
+        channel.BasicConsume(consumer: consumer,
+                             queue: replyQueueName,
+                             autoAck: true);
+    }
+
+    public Task<string> CallAsync(string message, CancellationToken cancellationToken = default)
+    {
+        IBasicProperties props = channel.CreateBasicProperties();
+        var correlationId = Guid.NewGuid().ToString();
+        // Define correlation ID
+        props.CorrelationId = correlationId;
+        // Define reply queue
+        props.ReplyTo = replyQueueName;
+        var messageBytes = Encoding.UTF8.GetBytes(message);
+        var tcs = new TaskCompletionSource<string>();
+        callbackMapper.TryAdd(correlationId, tcs);
+
+        // Publish message to rpc queue
+        channel.BasicPublish(exchange: string.Empty,
+                             routingKey: QUEUE_NAME,
+                             // Providing callback routing key and correlation ID
+                             basicProperties: props,
+                             body: messageBytes);
+
+        cancellationToken.Register(() => callbackMapper.TryRemove(correlationId, out _));
+        return tcs.Task;
+    }
+
+    public void Dispose()
+    {
+        // closing a connection will also close all channels on it
+        connection.Close();
+    }
+}
+
+public class Rpc
+{
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("RPC Client");
+        string n = args.Length > 0 ? args[0] : "30";
+        await InvokeAsync(n);
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+
+    private static async Task InvokeAsync(string n)
+    {
+        using var rpcClient = new RpcClient();
+
+        Console.WriteLine(" [x] Requesting fib({0})", n);
+        var response = await rpcClient.CallAsync(n);
+        Console.WriteLine(" [.] Got '{0}'", response);
+    }
+}
--- 6_RPC/RPCServer/Program.cs
+++ 6_RPC/RPCServer/Program.cs
@@ -1,2 +1,83 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// https://www.rabbitmq.com/tutorials/tutorial-six-dotnet
+
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+// Create connection to the server
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+// Declare queue to consume from
+channel.QueueDeclare(queue: "rpc_queue",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+// Ensure message is only sent to worker when it is not busy
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+// Start a basic content-class consumer
+var consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume(queue: "rpc_queue",
+                     // Set auto acknowledgment to false
+                     autoAck: false,
+                     consumer: consumer);
+Console.WriteLine(" [x] Awaiting RPC requests");
+
+// Declare callback to handle incoming messages
+consumer.Received += (model, ea) =>
+{
+    string response = string.Empty;
+
+    var body = ea.Body.ToArray();
+    var props = ea.BasicProperties;
+    var replyProps = channel.CreateBasicProperties();
+    // Set reply correlation ID to match request
+    replyProps.CorrelationId = props.CorrelationId;
+
+    try
+    {
+        // Calculate fibonacci number
+        var message = Encoding.UTF8.GetString(body);
+        int n = int.Parse(message);
+        Console.WriteLine($" [.] Fib({message})");
+        response = Fib(n).ToString();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($" [.] {e.Message}");
+        response = string.Empty;
+    }
+    finally
+    {
+        // Publish message 
+        var responseBytes = Encoding.UTF8.GetBytes(response);
+        channel.BasicPublish(exchange: string.Empty,
+                             // Callback routing key provided by client
+                             routingKey: props.ReplyTo,
+                             // Provide correlation ID to client
+                             basicProperties: replyProps,
+                             body: responseBytes);
+
+        // Send acknowledgement
+        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+    }
+};
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+// Assumes only valid positive integer input.
+// Don't expect this one to work for big numbers, and it's probably the slowest recursive implementation possible.
+static int Fib(int n)
+{
+    if (n is 0 or 1)
+    {
+        return n;
+    }
+
+    return Fib(n - 1) + Fib(n - 2);
+}
